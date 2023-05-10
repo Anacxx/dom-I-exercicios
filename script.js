0f426f8ab951355389aedee13da6d64f2b99c91e
Anacxx <anacarolina.clcx@gmail.com>
2023-05-10 09:18:07 -0300

    as

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const frutas = ["laranja", "limão", "uva"];
+console.log(frutas[0])
+const item1 = document.getElementById('fruta-1')
+item1.innerHTML += frutas[0]
+const item2 = document.getElementById('fruta-2')
+item2.innerHTML += frutas[1]
+const item3 = document.getElementById('fruta-3')
+item3.innerHTML += frutas[2]
+
